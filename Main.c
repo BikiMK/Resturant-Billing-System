4bc4c0ced4012826fe9c5aae49b1c9bf069e3b82
Biki Mukherjee <139540910+BikiMK@users.noreply.github.com>
2024-09-26 11:55:08 +0530

    Main.c

--- Main.c
+++ Main.c
@@ -1,407 +1,182 @@
 #include<stdio.h>
+#include<string.h>
 #include<stdlib.h>
-int again;
-int total=0;
-float gst,cgst,sgst;
-int bfrate[7][2]={  {0,30},
-                    {1,35},
-                    {2,40},
-                    {3,20},
-                    {4,15},
-                    {5,20},
-                    {6,30}
-                };
-int lunchrate[7][2]={{0,80},
-                {1,120},
-                {2,20},
-                {3,120},
-                {4,150},
-                {5,160},
-                {6,30}};
-int dinner_rate[7][2]={{0,65},
-                {1,50},
-                {2,70},
-                {3,80},
-                {4,65},
-                {5,110},
-                {6,50}};
-int purchased[][3]={{0,0},
-                        {0,0},
-                        {0,0},
-                        {0,0},
-                        {0,0},
-                        {0,0},
-                        {0,0}
-                        };
-int temp[][3]={{0,0},
-                        {0,0},
-                        {0,0},
-                        {0,0},
-                        {0,0},
-                        {0,0},
-                        {0,0}
-                        };
-void bfast(char bfmenu[][100]);
-void lunch( char lunchmenu[][100]);
-void dinner(char dinnermenu[][100]);
-void main_menu();
-void display();
-void lunch_menucard();
-void dinner_menu();
-void display_bf_bill(char bfmenu[][100]);
-void display_lunch_bill(char lunchmenu[][100]);
-void display_dinner_bill(char dinnermenu[][100]);
-void display_bfmenu();
-void repeatbf(char bfmenu[][100]);
-void repeatlunch(char lunchmenu[][100]);
-void repeatdinner(char dinnermenu[][100]);
-void display_total_words();
-void main()
-{
-    char bfmenu[][100]={"toast","Idli-wada","Dosa","Upma","milk","tea","coffee"};
-    char lunchmenu[][100]={"Veg Biryani","Special meal","Roti","Dal","Paneer Tikka","Veg mix","Icecream"};
-    char dinnermenu[][100]={"Fried rice","Spegatti","Burger"};
-    char choice;
-    do
-    {
-        enter:
-        display();
-        printf("Enter your choice here : ");
-        scanf("%c", &choice);
-        getchar();
-        switch(choice)
-        {
-            case 'A':
-            case 'a':printf("\nBreakfast\n");
-                    bfast(bfmenu);
-                    break;
-            case 'B':
-             case 'b':printf("\nLunch\n");
-                    lunch(lunchmenu);
-                    break;
-             case 'C':
-            case 'c':printf("\nDinner\n");
-                    dinner(dinnermenu);
-                    break;
-           default:printf("\nWrong choice entered Please enter the valid choice!!\n");
-                    goto enter;
-        }
-    }while(choice!='d');
-}
-void display()
-{
-    printf("                Welcome to MY Restaurant.          \n ");
-    printf("             +============================+          \n\n");
-    printf("  && Please select the meal that you would like to purchase. && \n\n");
-    printf("\t\t      [A] Breakfast\n");
-    printf("\t\t      [B] Lunch\n");
-    printf("\t\t      [C] Dinner\n");
-}
-void display_bfmenu()
-{
-    printf("                Welcome to MY Restaurant.          \n ");
-    printf("             +============================+          \n\n");
-    printf("                  $  Breakfast Menu  $ \n\n");
-    printf("  && Please select the food that you would like to purchase. && \n\n");
-    printf("\t\t   [0] Toast - Rs 30.00\n");
-    printf("\t\t   [1] Idli-Vada - Rs 35.00\n");
-    printf("\t\t   [2] Dosa - Rs 40.00\n");
-    printf("\t\t   [3] UPMA Rs 20.00\n");
-    printf("\t\t   [4] Milk- Rs 15.00\n");
-    printf("\t\t   [5] Tea - Rs 20.00\n");
-    printf("\t\t   [6] Coffee - Rs 30.00\n");
-}
-void bfast(char bfmenu[][100]) //Breakfast Menu Screen
-{
-    int choice = 0; //local variables
-    int quantity = 0;
-    int again = 0,amt=0;
-    int code,i;
-    display_bfmenu();
-    printf("\nEnter the code:: ");
-    scanf("%d", &code);
-    getchar();
-    printf("Total is %d\n\n",total);
-    if(code>=0&&code<=6)
-    {
-        printf("\nEnter the quantity::");
-        scanf("%d",&quantity);
-        purchased[code][1]= quantity*bfrate[code][1]; /*purchased[code][1]+ (quantity*bfrate[code][1]);*/
-        total+=purchased[code][ 1];
-        printf("Total is %d\n\n",total);
-        gst=total*0.12;
-        cgst=gst/2;
-        sgst=cgst;
-    }
-    else
-    {
-        printf("\nInvalid code entered, Please enter the code again!!!!\n\n");
-        bfast(bfmenu);
-    }
-    repeatbf(bfmenu);
-}
-void repeatbf(char bfmenu[][100])
-{
-    printf("\nWould you like to buy anything else?\n[1] Yes , [2] No : "); // Allows user to choose whether to check-out or buy anything else.
-    scanf("%d",&again);
-    if (again == 1)
-            bfast(bfmenu);
-    else if (again == 2 )
-    {
-        display_bf_bill(bfmenu);
-        exit(0);
-    }
-    else
-    {
-        printf("\n\n\t\tSorry Invalid Decision Entered\n");
-        repeatbf(bfmenu);
-    }
-}
-void display_bf_bill(char bfmenu[][100])
-{
-    int i;
-    printf("  +~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~+          \n\n");
-    printf("                      MY RESTAURANT                   \n");
-    printf("                    BILLING INFORMATION                \n");
-    printf("\t\t ITEMS\t\tPrice(in Rs.)\n");
-    for(i=0;i<7;i++)
-    {
-        if(purchased[i][1]!=0)
-        {
-            printf("\t\t%s\t\t",bfmenu[i]);
-            printf("%d\n",purchased[i][1]); /*purchased[i][1]);*/
-        }
-    }
-    printf("\t\tGST=Rs.%0.2f\n",gst);
-    printf("\t\tC-GST=Rs.%0.2f\n",cgst);
-    printf("\t\tS-GST=Rs.%0.2f\n",sgst);
-    printf("\t\tTotal=Rs.%d\n",total);
-    display_total_words();
-    printf("\n\n  +~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~+          \n\n");
 
+struct items{
+    char item[20];
+    float price;
+    int qty;
+};
+
+struct orders{
+    char customer[50];
+    char date[50];
+    int numOfItems;
+    struct items itm[50];
+};
+//functions to generate biils
+void generateBillHeader(char name[50],char date[30]){
+    printf("\n\n");
+        printf("\t    ADV. Restaurant");
+        printf("\n\t   -----------------");
+        printf("\nDate:%s",date);
+        printf("\nInvoice To: %s",name);
+        printf("\n");
+        printf("---------------------------------------\n");
+        printf("Items\t\t");
+        printf("Qty\t\t");
+        printf("Total\t\t");
+        printf("\n---------------------------------------");
+        printf("\n\n");
 }
-void display_lunch_bill(char lunchmenu[][100])
-{
-    int i;
-    printf("  +~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~+          \n\n");
-    printf("                      MY RESTAURANT                   \n");
-    printf("                    BILLING INFORMATION                \n");
-    printf("\t\t ITEMS\t\t\tPrice(in Rs.)\n");
-    for(i=0;i<7;i++)
-    {
-        if(purchased[i][1]!=0)
-        {
-            printf("\t\t %s \t\t",lunchmenu[i]);
-            printf("%d\n",purchased[i][1]);
-        }
-    }
-    printf("\t\tGST=Rs.%0.2f\n",gst);
-    printf("\t\tC-GST=Rs.%0.2f\n",cgst);
-    printf("\t\tS-GST=Rs.%0.2f\n",sgst);
-    printf("\t\tTotal=Rs.%d\n",total);
-    display_total_words();
-    printf("\n\n  +~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~+          \n\n");
+void generateBillBody(char item[30],int qty, float price){
+    printf("%s\t\t",item); 
+        printf("%d\t\t",qty); 
+        printf("%.2f\t\t",qty * price); 
+        printf("\n");
 }
-void display_dinner_bill(char dinnermenu[][100])
-{
-    int i;
-    printf("  +~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~+          \n\n");
-    printf("                      MY RESTAURANT                   \n");
-    printf("                    BILLING INFORMATION                \n");
-    printf("\t\t ITEMS\t\t\tPrice(in Rs.)\n");
-    for(i=0;i<7;i++)
-    {
-        if(purchased[i][1]!=0)
-        {
-            printf("\t\t %s \t\t",dinnermenu[i]);
-            printf("%d\n",purchased[i][1]);
+
+
+
+void generateBillFooter(float total){
+    printf("\n");
+    float dis = 0.1*total;
+    float netTotal=total-dis;
+    float cgst=0.09*netTotal,grandTotal=netTotal + 2*cgst;//netTotal + cgst + sgst
+    printf("---------------------------------------\n");
+    printf("Sub Total\t\t\t%.2f",total);
+    printf("\nDiscount @10%s\t\t\t%.2f","%",dis);
+    printf("\n\t\t\t\t-------");
+    printf("\nNet Total\t\t\t%.2f",netTotal);
+    printf("\nCGST @9%s\t\t\t%.2f","%",cgst);
+    printf("\nSGST @9%s\t\t\t%.2f","%",cgst);
+    printf("\n---------------------------------------");
+    printf("\nGrand Total\t\t\t%.2f",grandTotal);
+    printf("\n---------------------------------------\n");
+}
+int main(){
+    
+    int opt,n;
+    struct orders ord;
+    struct orders order;
+    char saveBill = 'y',contFlag = 'y';
+    char name[50];
+    FILE *fp;
+       //dashboard
+    while(contFlag == 'y'){
+    system("clear");
+    float total = 0;
+    int invoiceFound = 0;
+    printf("\t============ADV. RESTAURANT============");
+    printf("\n\nPlease select your prefered operation");
+    printf("\n\n1.Generate Invoice");
+    printf("\n2.Show all Invoices");
+    printf("\n3.Search Invoice");
+    printf("\n4.Exit");
+
+    printf("\n\nYour choice:\t");
+    scanf("%d",&opt);
+    fgetc(stdin);
+    switch(opt){
+        case 1:
+        system("clear");
+        printf("\nPlease enter the name of the customer:\t");
+        fgets(ord.customer,50,stdin);
+        ord.customer[strlen(ord.customer)-1] = 0;
+        strcpy(ord.date,__DATE__);
+        printf("\nPlease enter the number of items:\t");
+        scanf("%d",&n);
+        ord.numOfItems = n;
+        for(int i=0;i<n;i++){
+            fgetc(stdin);
+            printf("\n\n");
+            printf("Please enter the item %d:\t",i+1);
+            fgets(ord.itm[i].item,20,stdin);
+            ord.itm[i].item[strlen(ord.itm[i].item)-1]=0;
+            printf("Please enter the quantity:\t");
+            scanf("%d",&ord.itm[i].qty);
+            printf("Please enter the unit price:\t");
+            scanf("%f",&ord.itm[i].price);
+            total += ord.itm[i].qty * ord.itm[i].price;
         }
 
-    }
-    printf("\t\tGST=Rs.%0.2f\n",gst);
-    printf("\t\tC-GST=Rs.%0.2f\n",cgst);
-    printf("\t\tS-GST=Rs.%0.2f\n",sgst);
-    printf("\t\tTotal=Rs.%d\n\n",total);
-    display_total_words();
-    printf("\n\n  +~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~+          \n\n");
-
-void lunch(char lunchmenu[][100]) // Lunch Screen Menu
-{
-    int choice = 0; //local variables
-    int quantity = 0;
-    int again = 0,amt=0;
-    int code,i;
-    lunch_menucard();
-    printf("\nEnter the code:: ");
-    scanf("%d", &code);
-    getchar();
-    if(code>=0&&code<=6)
-    {
-        printf("\nEnter the quantity::");
-        scanf("%d",&quantity);
-       // purchased[code][1]= purchased[code][1]+(quantity*lunchrate[code][1]);
-        purchased[code][1]= quantity*lunchrate[code][1];
-         total+=purchased[code][ 1];
-        gst=total*0.12;
-        cgst=gst/2;
-        sgst=cgst;
-    }
-    else
-    {
-        printf("\nInvalid code entered, Please enter the code again!!!!\n\n");
-         lunch(lunchmenu);
-    }
-    repeatlunch(lunchmenu);
-}
-void dinner(char dinnermenu[][100]) // Dinner Menu Screen
-{
-    int choice = 0; //local variables
-    int quantity = 0;
-    int again = 0,amt=0;
-    int code,i;
-    dinner_menu();
-
-    printf("\nEnter the code:: ");
-    scanf("%d", &code);
-    getchar();
-    if(code>=0&&code<=6)
-    {
-        printf("\nEnter the quantity::");
-        scanf("%d",&quantity);
-       // purchased[code][1]= purchased[code][1]+(quantity*dinner_rate[code][1]);
-        purchased[code][1]= quantity*dinner_rate[code][1];
-        total+=purchased[code][1];
-        gst=total*0.12;
-        cgst=gst/2;
-        sgst=cgst;
-    }
-    else
-    {
-        printf("\nInvalid code entered, Please enter the code again!!!!\n\n");
-        dinner(dinnermenu);
-    }
-    repeatdinner(dinnermenu);
-}
-void lunch_menucard()
-{
-    printf("                Welcome to MY Restaurant.          \n ");
-    printf("             +============================+          \n\n");
-    printf("                   $  Lunch Menu  $ \n\n");
-    printf("  && Please select the food that you would like to purchase. && \n\n");
-    printf("\t\t   [0] Veg Biryani - Rs 80.00\n");
-    printf("\t\t   [1] Special Meal- Rs 120.00\n");
-    printf("\t\t   [2] Roti - Rs 20.00\n");
-    printf("\t\t   [3] Dal tadka - Rs 120.00\n");
-    printf("\t\t   [4] Paneer Tikka - Rs 150.00\n");
-    printf("\t\t   [5] Veg Mix - Rs 160.00\n");
-    printf("\t\t   [6] Ice cream - Rs 30.00\n");
-    printf("Enter your choice here : ");
-}
-void dinner_menu()
-{
-    printf("                Welcome to MY Restaurant.          \n ");
-    printf("             +============================+          \n\n");
-    printf("                    $  Dinner Menu  $ \n\n");
-    printf("  && Please select the food that you would like to purchase. && \n\n");
-    printf("\t\t   [0] Fried Rice - Rs 65.00\n");
-    printf("\t\t   [1] Spagetti- Rs50\n");
-    printf("\t\t   [2] Burger - Rs 70.00\n");
-    printf("\t\t   [3] Pasta - Rs 80.00\n");
-    printf("\t\t   [4] Noodles - Rs 65.00\n");
-    printf("\t\t   [5] Paratha - Rs 110.00\n");
-    printf("\t\t   [6] Fruit Salad - Rs 50.00\n");
-}
-void repeatlunch(char lunchmenu[][100])
-{
-    printf("\nWould you like to buy anything else?\n[1] Yes , [2] No : "); // Allows user to choose whether to check-out or buy anything else.
-    scanf("%d",&again);
-    if (again == 1)
-            lunch(lunchmenu);
-    else if (again == 2 )
-    {
-        display_lunch_bill(lunchmenu);
-        exit(0);
-    }
-    else
-    {
-        printf("\n\n\t\tSorry Invalid Decision Entered\n");
-        repeatlunch(lunchmenu);
-    }
-}
-void repeatdinner(char dinnermenu[][100])
-{
-
-    printf("\nWould you like to buy anything else?\n[1] Yes , [2] No : "); // Allows user to choose whether to check-out or buy anything else.
-    scanf("%d",&again);
-    if (again == 1)
-    {
-        dinner(dinnermenu);
-    }
-    else if (again == 2 )
-    {
-        display_dinner_bill(dinnermenu);
-        exit(0);
-    }
-    else
-    {
-        printf("\n\n\t\tSorry Invalid Decision Entered\n");
-        repeatdinner(dinnermenu);
-    }
-}
-void display_total_words()
-{
-    int num=0,digits=0,temp=0;
-    while(total != 0)
-    {
-        num=(num * 10)+(total % 10);
-        total/= 10;
-        digits++;
-    }
-    printf("\nNo of digits::%d\n",digits);
-    while(num!= 0)
-    {
-        temp++;
-        switch(num%10)
-        {
-            case 0:
-                printf("Zero ");
-                break;
-            case 1:
-                printf("One ");
-                break;
-            case 2:
-                printf("Two ");
-                break;
-            case 3:
-                printf("Three ");
-                break;
-            case 4:
-                printf("Four ");
-                break;
-            case 5:
-                printf("Five ");
-                break;
-            case 6:
-                printf("Six ");
-                break;
-            case 7:
-                printf("Seven ");
-                break;
-            case 8:
-                printf("Eight ");
-                break;
-            case 9:
-                printf("Nine ");
-                break;
+        generateBillHeader(ord.customer,ord.date);
+        for(int i=0;i<ord.numOfItems;i++){
+            generateBillBody(ord.itm[i].item,ord.itm[i].qty,ord.itm[i].price);
         }
-        num = num / 10;
+        generateBillFooter(total);
+
+        printf("\nDo you want to save the invoice [y/n]:\t");
+        scanf("%s",&saveBill);
+
+        if(saveBill == 'y'){
+            fp = fopen("RestaurantBill.dat","a+");
+            fwrite(&ord,sizeof(struct orders),1,fp);
+            if(fwrite != 0)
+            printf("\nSuccessfully saved");
+            else 
+            printf("\nError saving");
+            fclose(fp);
+        }
+        break;
+
+        case 2:
+        system("clear");
+        fp = fopen("RestaurantBill.dat","r");
+        printf("\n  *****Your Previous Invoices*****\n");
+        while(fread(&order,sizeof(struct orders),1,fp)){
+            float tot = 0;
+            generateBillHeader(order.customer,order.date);
+            for(int i=0;i<order.numOfItems;i++){
+                generateBillBody(order.itm[i].item,order.itm[i].qty,order.itm[i].price);
+                tot+=order.itm[i].qty * order.itm[i].price;
+            }
+            generateBillFooter(tot);
+        }
+        fclose(fp);
+        break;
+
+        case 3:
+        printf("Enter the name of the customer:\t");
+        //fgetc(stdin);
+        fgets(name,50,stdin);
+        name[strlen(name)-1] = 0;
+        system("clear");
+        fp = fopen("RestaurantBill.dat","r");
+        printf("\t*****Invoice of %s*****",name);
+        while(fread(&order,sizeof(struct orders),1,fp)){
+            float tot = 0;
+            if(!strcmp(order.customer,name)){
+            generateBillHeader(order.customer,order.date);
+            for(int i=0;i<order.numOfItems;i++){
+                generateBillBody(order.itm[i].item,order.itm[i].qty,order.itm[i].price);
+                tot+=order.itm[i].qty * order.itm[i].price;
+            }
+            generateBillFooter(tot);
+            invoiceFound = 1;
+            }
+        
+        }
+        if(!invoiceFound){
+            printf("Sorry the invoice for %s doesnot exists",name);
+        }
+        fclose(fp);
+        break;
+
+    case 4:
+    printf("\n\t\t Bye Bye :)\n\n");
+    exit(0);
+    break;
+
+    default:
+    printf("Sorry invalid option");
+    break;
     }
-    if(num==0&&digits!=temp){
-	
-        printf(" Zero ");
-    return;}
+    printf("\nDo you want to perform another operation?[y/n]:\t");
+    scanf("%s",&contFlag);
+    }
+    printf("\n\t\t Bye Bye :)\n\n");
+    printf("\n\n");
 
-
-
-
-
+    return 0;
+}
